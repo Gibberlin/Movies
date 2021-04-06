42c717313b7eb6d6325188a59253642aa6a1796b
Syed Yashin Hussain <syedeyashin@gmail.com>
2021-04-06 21:11:30 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import { Switch, route, Route } from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route exact path="/">
+        This is home page
+      </Route>
+
+      <Route exact path="/started">
+        {' '}
+        This app has started
+      </Route>
+      <Route>
+        This Page is not found
+        <h1>Error: 404</h1>
+      </Route>
+    </Switch>
   );
 }
 
