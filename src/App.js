e06c34728cb2ac9394c981702aa02e85f893bd88
Syed Yashin Hussain <syedeyashin@gmail.com>
2021-04-06 21:59:32 +0530

    added home,nav and started components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React from 'react';
-import { Switch, route, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
+import Nav from './components/Nav';
+import Home from './components/Home';
+import Started from './components/Started';
+
 function App() {
   return (
-    <Switch>
-      <Route exact path="/">
-        This is home page
-      </Route>
+    <div>
+      <Nav />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
 
-      <Route exact path="/started">
-        {' '}
-        This app has started
-      </Route>
-      <Route>
-        This Page is not found
-        <h1>Error: 404</h1>
-      </Route>
-    </Switch>
+        <Route exact path="/started">
+          <Started />
+        </Route>
+        <Route>
+          This Page is not found
+          <h1>Error: 404</h1>
+        </Route>
+      </Switch>
+    </div>
   );
 }
 
---
+++ src/components/Started.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Started = () => {
+  return (
+    <div>
+      <h1>The app have started</h1>
+    </div>
+  );
+};
+
+export default Started;
