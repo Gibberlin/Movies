5f7887f1879ace1c46cab47a3219e413adfad44e
Syed Yashin Hussain <syedeyashin@gmail.com>
2021-04-07 12:28:38 +0530

    added search and fetch results

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MainPageLayout from './MainPageLayout';
 
 const Home = () => {
+  const [input, setinput] = useState('');
+
+  const inputOnChange = ev => setinput(ev.target.value);
+
+  const onSearch = () => {
+    fetch(`http://api.tvmaze.com/search/shows?q=${input}`)
+      .then(r => r.json())
+      .then(results => {
+        console.log(results);
+      });
+  };
+  const onKeyDown = ev => {
+    if (ev.keyCode === 13) {
+      onSearch();
+    }
+  };
+
   return (
     <div>
       <MainPageLayout>
-        <h1>This is home</h1>
+        <input
+          type="text"
+          onChange={inputOnChange}
+          onKeyDown={onKeyDown}
+          value={input}
+        />
+        <button type="button" onClick={onSearch}>
+          Search
+        </button>
       </MainPageLayout>
     </div>
   );
