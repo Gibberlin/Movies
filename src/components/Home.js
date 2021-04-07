1a90ce6d43a2ee45cef75735741b376428311880
Syed Yashin Hussain <syedeyashin@gmail.com>
2021-04-07 18:18:16 +0530

    sorted out the api into a new file

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,45 @@
 import React, { useState } from 'react';
 import MainPageLayout from './MainPageLayout';
+import { apiGet } from '../misc/config';
 
 const Home = () => {
   const [input, setinput] = useState('');
 
   const inputOnChange = ev => setinput(ev.target.value);
 
+  const [results, setResults] = useState(null);
+
   const onSearch = () => {
-    fetch(`http://api.tvmaze.com/search/shows?q=${input}`)
-      .then(r => r.json())
-      .then(results => {
-        console.log(results);
-      });
+    apiGet(`/search/shows?q=${input}`).then(result => {
+      setResults(result);
+      console.log(result);
+    });
   };
+
   const onKeyDown = ev => {
     if (ev.keyCode === 13) {
       onSearch();
     }
+  };
+
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return (
+        <div>
+          <h1>the results was not found</h1>;
+        </div>
+      );
+    }
+    if (results && results.length > 0) {
+      return (
+        <div>
+          {results.map(item => (
+            <div key={item.show.id}>{item.show.name}</div>
+          ))}
+        </div>
+      );
+    }
+    return null;
   };
 
   return (
@@ -31,6 +54,7 @@ const Home = () => {
         <button type="button" onClick={onSearch}>
           Search
         </button>
+        {renderResults()}
       </MainPageLayout>
     </div>
   );
