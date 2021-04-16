82eb95b1083efb4f77e605c9bb0c860be0bac7df
Syed Yashin Hussain <syedeyashin@gmail.com>
2021-04-16 19:31:13 +0530

    styled radio button and home page

--- src/App.js
+++ src/App.js
@@ -1,29 +1,40 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 
 import Home from './pages/Home';
 import Starred from './pages/Starred';
 import Show from './pages/Show';
 
+const theme = {
+  mainColors: {
+    blue: '#9fcc2e',
+    gray: '#5a6650',
+    dark: '#0e402d',
+  },
+};
+
 function App() {
   return (
-    <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
+    <ThemeProvider theme={theme}>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
 
-      <Route exact path="/starred">
-        <Starred />
-      </Route>
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
 
-      <Route exact path="/show/:id">
-        <Show />
-      </Route>
+        <Route exact path="/show/:id">
+          <Show />
+        </Route>
 
-      <Route>
-        <div>Not found</div>
-      </Route>
-    </Switch>
+        <Route>
+          <div>Not found</div>
+        </Route>
+      </Switch>
+    </ThemeProvider>
   );
 }
 
